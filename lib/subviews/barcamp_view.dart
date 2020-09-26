aedd713f8559bcd17e6ea521927fac12906990da
Nicolas Wazulek <nicolas.wazulek@niwa-dev.de>
2020-09-26 16:42:47 +0200

    Implemented event cards for hackerton and barcamp

--- lib/subviews/barcamp_view.dart
+++ lib/subviews/barcamp_view.dart
@@ -1,10 +1,28 @@
 import 'package:event_view/subviews/subview.dart';
+import 'package:event_view/widgets/event_card/event_card_model.dart';
+import 'package:event_view/widgets/event_card/event_card_view.dart';
 import 'package:flutter/material.dart';
 
 class BarcampMainView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(color: Colors.blue);
+    return ListView(
+      padding: EdgeInsets.all(4.0),
+      children: <Widget>[
+        for (int i = 0; i < 6; i++)
+          EventCard.presentation(
+              new Event(
+                "Example Talk ${i + 1}",
+                "I like trains",
+                'https://lh3.googleusercontent.com/VHB9bVB8cTcnqwnu0nJqKYbiutRclnbGxTpwnayKB4vMxZj8pk1220Rg-6oQ68DwAkqO',
+              ),
+              "Speaker",
+              "Testfirma",
+              "09:00 - 10:00",
+              "Raum 1",
+              i == 0),
+      ],
+    );
   }
 }
 
--- lib/subviews/talks_view.dart
+++ lib/subviews/talks_view.dart
@@ -6,10 +6,6 @@ import 'package:flutter/material.dart';
 class TalksMainView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    List<CardDescription> description = new List<CardDescription>();
-    description.add(new CardDescription(Icons.info_outline, "Test"));
-    description.add(new CardDescription(Icons.access_time, "4:20"));
-    description.add(new CardDescription(Icons.place, "Raum 01"));
     return ListView(
       padding: EdgeInsets.all(4.0),
       children: <Widget>[
