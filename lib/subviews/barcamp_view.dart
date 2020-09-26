aaa2de262180f0e0523a8d76c079de2cd88fe47b
Nicolas Wazulek <nicolas.wazulek@niwa-dev.de>
2020-09-26 17:35:38 +0200

    tried to implement barcamp area to backend implementation, but doesn't work yet

--- lib/subviews/barcamp_view.dart
+++ lib/subviews/barcamp_view.dart
@@ -1,28 +1,31 @@
+import 'package:event_view/hackerkisteContext.dart';
 import 'package:event_view/subviews/subview.dart';
 import 'package:event_view/widgets/event_card/event_card_model.dart';
 import 'package:event_view/widgets/event_card/event_card_view.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class BarcampMainView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return ListView(
-      padding: EdgeInsets.all(4.0),
-      children: <Widget>[
-        for (int i = 0; i < 6; i++)
-          EventCard.presentation(
-              new Event(
-                "Example Talk ${i + 1}",
-                "I like trains",
-                'https://lh3.googleusercontent.com/VHB9bVB8cTcnqwnu0nJqKYbiutRclnbGxTpwnayKB4vMxZj8pk1220Rg-6oQ68DwAkqO',
-              ),
-              "Speaker",
-              "Testfirma",
-              "09:00 - 10:00",
-              "Raum 1",
-              i == 0),
-      ],
-    );
+    return Consumer<HackerkisteContext>(
+        builder: (context, hackerkiste, child) => ListView(
+              padding: EdgeInsets.all(4.0),
+              children: <Widget>[
+                for (var item in hackerkiste.getBarcamps())
+                  EventCard.presentation(
+                      new Event(
+                        item.title,
+                        item.description,
+                        "",
+                      ),
+                      item.organizerName,
+                      item.organizerCompany,
+                      "${item.start.hour}:${item.start.minute} - ${item.end.hour}:${item.end.minute}",
+                      item.room,
+                      false),
+              ],
+            ));
   }
 }
 
